4f8f78f0970cc6d933c718e1355cc2bbc45e5ce3
HipMaze <ayman.elazm.dev@gmail.com>
2022-11-22 11:38:14 +0100

    defining jwt strategy (wip)

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -1,17 +1,91 @@
-import express from "express";
+const express = require("express");
+const { to } = require("await-to-js");
+const { verifyPassword, hashPassword } = require("../auth/utils");
+const { login } = require("../auth/strategies/jwt");
+const { createUser, getUserByEmail } = require("../config/user/getter");
 
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
 	const { email, password } = req.body;
+	const [err, user] = await to(getUserByEmail(email));
 
-	return res.status(200).json({ success: true, data });
+	const authenticationError = () => {
+		return res
+			.status(500)
+			.json({ success: false, data: "Authentication error!" });
+	};
+
+	if (!(await verifyPassword(password, user.password))) {
+		console.error("Passwords do not match");
+		return authenticationError();
+	}
+
+	const [loginErr, token] = await to(login(req, user));
+
+	if (loginErr) {
+		console.error("Log in error", loginErr);
+		return authenticationError();
+	}
+
+	return res
+		.status(200)
+		.cookie("jwt", token, {
+			httpOnly: true,
+		})
+		.json({
+			success: true,
+			data: "/",
+		});
 });
 
 router.post("/register", async (req, res) => {
 	const { firstName, lastName, email, password } = req.body;
 
-	return res.status(200).json({ success: true, data: null });
+	if (!/\b\w+\@\w+\.\w+(?:\.\w+)?\b/.test(email)) {
+		return res
+			.status(500)
+			.json({ success: false, data: "Enter a valid email address." });
+	} else if (password.length < 5 || password.length > 20) {
+		return res.status(500).json({
+			success: false,
+			data: "Password must be between 5 and 20 characters.",
+		});
+	}
+
+	let [err, user] = await to(
+		createUser({
+			firstName,
+			lastName,
+			email,
+			password: await hashPassword(password),
+		})
+	);
+
+	if (err) {
+		return res
+			.status(500)
+			.json({ success: false, data: "Email is already taken" });
+	}
+
+	const [loginErr, token] = await to(login(req, user));
+
+	if (loginErr) {
+		console.error(loginErr);
+		return res
+			.status(500)
+			.json({ success: false, data: "Authentication error!" });
+	}
+
+	return res
+		.status(200)
+		.cookie("jwt", token, {
+			httpOnly: true,
+		})
+		.json({
+			success: true,
+			data: "/",
+		});
 });
 
-export default router;
+module.exports = router;
